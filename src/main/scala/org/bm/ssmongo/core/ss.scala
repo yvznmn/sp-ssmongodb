ded30891b35429af065881a2416cb701b8f7bca8
yavuz_karabiyik <yavuz.karabiyik@spglobal.com>
2022-02-16 17:15:37 -0500

    functions are written

--- src/main/scala/org/bm/ssmongo/core/ss.scala
+++ src/main/scala/org/bm/ssmongo/core/ss.scala
@@ -4,32 +4,30 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{FloatType, StringType, StructField, StructType}
 import org.apache.spark.sql.streaming.Trigger
 
+import java.util.Properties
+import org.apache.log4j.PropertyConfigurator
+
+
 //unit_test, dependency_injection, logging, schemas_in_mongo_as_json
 
 object ss {
 
   print("Start")
 
-//  val client = MongoClient()
-//  val db:MongoDatabase = client.getDatabase("my_db")
-//  db.createCollection("my_col")
-//  val my_col = db.getCollection("my_col")
 
-  val mongoURL = "mongodb://127.0.0.1/ssmongo.ssmongocol"
-  val my_source_path = "src/main/scala/org/bm/ssmongo/testData"
-  val my_target_path = "src/main/scala/org/bm/ssmongo/target"
-  val my_checkpoint_path = "src/main/scala/org/bm/ssmongo/target/checkpoint"
+  def read_stream_csv_file(spark:SparkSession, file_type: String, header_value: Boolean, table_schema:StructType, source_path:String): DataFrame = {
+    val df = spark.readStream
+      .format(file_type)
+      .option("header", header_value)
+      .schema(table_schema)
+      .load(source_path)
 
-  val spark = SparkSession
-    .builder()
-    .master("local[1]")
-    .appName("SSMongo")
-    .config("spark.mongodb.input.uri", mongoURL)
-    .config("spark.mongodb.output.uri", mongoURL)
-    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1")
-    .getOrCreate()
+    println(df.isStreaming)
 
-  import spark.implicits._
+    df
+
+  }
+
 
   val my_schema = StructType(Array(
     StructField("STATION", StringType, true),
@@ -38,80 +36,62 @@ object ss {
     StructField("HPCP", FloatType, true)
   ))
 
-  var df = spark.readStream
-    .format("csv")
-    .option("header", true)
-//    .option("spark.executor.processTreeMetrics.enabled", false)
-    .schema(my_schema)
-//    .option("checkpointLocation", "/tmp/my_load/")
-    .load(my_source_path)
-//    .as[my_schema_class]
 
-
-  println(df.isStreaming)
-
-  def my_save(message:DataFrame, id:Long): Unit = {
+  def write_row(message:DataFrame, id:Long): Unit = {
     message.persist()
     message.write
       .format("mongo")
       .mode("append")
-//      .option("database", "db_name")
-//      .option("collection", "collection_name")
       .save()
     message.persist()
   }
 
 
 
-//    .option("checkpointLocation","src/main/scala/org/bm/ssmongo/target")
-//    .format("console")
-//    .format("mongo")
-//    .option("database", "ssmongo")
-//    .option("collection", "ssmongocol")
-//    .trigger(Trigger.ProcessingTime("2 seconds"))
-
-
-
-
-
-
-
-//  def write_mongo_row(batch_df:DataFrame,batch_id:Int): Unit = {
-//    val mongoURL = "mongodb://localhost:27017/my_db.my_col"
-//    batch_df.persist()
-//    batch_df.write
-//      .format("mongo")
-//      .mode("append")
-//      .option("uri", mongoURL)
-//      .save()
-//    batch_df.unpersist()
-//    ()
-//  }
-
-
-
-
-
-
-//  val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
-//    "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
-//
-//  my_col.insertOne(doc)
-//
-//  println(my_col.find().first())
-
-
-
-
-
-
-
-
   def main(args: Array[String]): Unit = {
 
-    println("main")
-    df.writeStream.foreachBatch(my_save _).trigger(Trigger.ProcessingTime("2 seconds")).option("checkpointLocation", my_checkpoint_path).start().awaitTermination()
-    //only command line args
+
+    val connection_to_prop = new Properties()
+    connection_to_prop.load(getClass().getResourceAsStream("/PROD/env.properties"))
+    PropertyConfigurator.configure(connection_to_prop)
+
+
+    val mongoURL = connection_to_prop.getProperty("mongoURL")
+    val my_source_path = connection_to_prop.getProperty("my_source_path")
+    val my_target_path = connection_to_prop.getProperty("my_target_path")
+    val my_checkpoint_path = connection_to_prop.getProperty("my_checkpoint_location_value")
+
+
+    val table_schema = StructType(Array(
+      StructField("STATION", StringType, true),
+      StructField("STATION_NAME", StringType, true),
+      StructField("DATE", StringType, true),
+      StructField("HPCP", FloatType, true)
+    ))
+
+    val spark = SparkSession
+      .builder()
+      .master(connection_to_prop.getProperty("master"))
+      .appName(connection_to_prop.getProperty("app_name"))
+      .config(connection_to_prop.getProperty("mongo_config_input_uri"), mongoURL)
+      .config(connection_to_prop.getProperty("mongo_config_output_uri"), mongoURL)
+      .config(connection_to_prop.getProperty("spark_config_jars_packages_key"), connection_to_prop.getProperty("spark_config_jars_packages_value"))
+      .getOrCreate()
+
+    val readstream_df = read_stream_csv_file(
+      spark,
+      connection_to_prop.getProperty("spark_readstream_csv_format"),
+      connection_to_prop.getProperty("spark_readstream_option_header_value").toBoolean,
+      table_schema,
+      my_source_path
+    )
+
+    readstream_df.writeStream
+      .foreachBatch(write_row _)
+      .trigger(Trigger.ProcessingTime(connection_to_prop.getProperty("trigger_processing_time")))
+      .option(connection_to_prop.getProperty("my_checkpoint_location_key"), my_checkpoint_path)
+      .start()
+      .awaitTermination()
 
 
   }
