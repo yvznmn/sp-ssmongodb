0f918f356e4c21e6cf57fbd13297866ba0f3bcc5
yavuz_karabiyik <yavuz.karabiyik@spglobal.com>
2022-05-24 16:27:47 -0400

    appfactory

--- src/main/scala/org/bm/ssmongo/factory/ApplicationFactory.scala
+++ src/main/scala/org/bm/ssmongo/factory/ApplicationFactory.scala
@@ -1,5 +1,14 @@
 package org.bm.ssmongo.factory
 
+import org.apache.spark.sql.SparkSession
+import org.bm.ssmongo.core.StreamingJob
+import org.bm.ssmongo.entities.GlobalConfigEnt
+import org.bm.ssmongo.job.StreamingFromKafkaToMongo
+
 object ApplicationFactory {
+
+  def get_streaming_application_instance(spark:SparkSession, params:GlobalConfigEnt): StreamingJob[GlobalConfigEnt] = {
+    new StreamingFromKafkaToMongo(spark, params)
+  }
 
 }
